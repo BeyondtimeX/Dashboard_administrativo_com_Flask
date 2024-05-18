f9102945e8492d555a1cce2f574395ea4954f8df
Bruno Almeida <bruno.desouzaads@GMAIL.com>
2024-05-18 10:02:29 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-class Config():
+class Config:
     CSRF_ENABLE = True
-    SECRET = 'E93D268B3C7D7EB1E5A93BA6CA51EF2EB1108DD4A5D68660012D501F7BC49B10F7C'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')  # Usar variável de ambiente para maior segurança
     TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     APP = None
@@ -11,14 +11,22 @@ class DevelopmentConfig(Config):
     DEBUG = True
     IP_HOST = 'localhost'
     PORT_HOST = 8000
-    URL_MAIN = 'http://%s/%s' % (IP_HOST, PORT_HOST)  # http://localhost:8000
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://tiago.silva2:de_m7jBG^WZ=@localhost:3306/dashboard_prof'
+    URL_MAIN = f'http://{IP_HOST}:{PORT_HOST}'  # Usar f-string para maior clareza
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+mysqlconnector://username:password@localhost:3306/dashboard_prof')
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI', 'sqlite:///:memory:')  # Usar SQLite em memória para testes
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+mysqlconnector://username:password@localhost:3306/dashboard_prof')
 
 app_config = {
-    'development': DevelopmentConfig(),
-    'testing': None
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
 }
 
-app_active = os.getenv('FLASK_ENV')
-if app_active is None:
-    app_active = 'development'
+app_active = os.getenv('FLASK_ENV', 'development')

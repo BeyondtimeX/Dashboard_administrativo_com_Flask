c4e50c1b7c2e79ea521082951e453cf5dc0d1c59
Bruno Almeida <bruno.desouzaads@GMAIL.com>
2024-05-18 09:45:42 -0300

    Update migrate.py

--- migrate.py
+++ migrate.py
@@ -2,19 +2,24 @@
 from flask import Flask
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from models import *
+from models import db, Role, User, State, DiseaseState, Patient, Disease
+from config import app_active, app_config
 
 config = app_config[app_active]
 
-Role().__init__()
-User().__init__()
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config_name.SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    return app
+
+app = create_app(config)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = SQLAlchemy(app)
-    migrate = Migrate(app, db)
-    manager = Manager(app)
-    manager.add_command('db', MigrateCommand)
     manager.run()
